104054bd743b9ac5b7eabb2b70057aa35f9b470e
Andres Munoz <andres.munoz.b@usach.cl>
2016-09-01 19:42:32 -0300

    actualizando

---
+++ funciones.h
@@ -0,0 +1,8 @@
+
+
+typedef struct pixel pixel_t;
+typedef struct imagen imagen_t;
+imagen_t* crearImagen(int fila,int columna);
+pixel_t* crearPixel();
+void cargarPixel(int r,int g, int b, pixel_t* pixel);
+int comparaPixel(pixel_t* pixel1,pixel_t* pixel2);
--- prueba.c
+++ prueba.c
@@ -1,1 +1,27 @@
-#include 
+#include <stdio.h>
+
+
+int main()
+
+{
+	FILE *iF;
+		
+		if ( (iF = fopen("imagenPrincipal.txt","r")) != NULL)
+		{		
+			int a, b;
+			fscanf(iF,"%d %d",&a,&b);
+			printf("%d %d\n",a,b );
+			fflush(stdin);
+			fscanf(iF,"%d %d",&a,&b);
+			printf("%d %d\n",a,b );
+			fflush(stdin);
+			fscanf(iF,"%d %d",&a,&b);
+			printf("%d %d\n",a,b );
+			fflush(stdin);
+			fclose(iF);			
+		
+		}
+		else
+			printf("Archivo no existe o no se pudo abrir\n");
+	return 0;
+}
