39836f67ea06fdfb2b0814bed803d7cbff8a7b37
Andres Munoz <andres.munoz.b@usach.cl>
2016-09-03 23:26:38 -0300

    arreglando funciones2.h

--- funciones2.h
+++ funciones2.h
@@ -1,3 +1,15 @@
 #include "estructuras.h"
+
+
 imagen_t* inicializarImagen(imagen_t* imagen,int fila, int columna);
 void liberarImagen(imagen_t* imagen);
+void imprimirPixel(pixel_t pixel);
+void imprimirImagen(imagen_t* imagen);
+void cargarPixel(int r,int g, int b, pixel_t* pixel);
+void cargarImagen(imagen_t* imagen,FILE* archivo);
+int compararPixel(pixel_t* pixel1,pixel_t* pixel2);
+int contarImagenes();
+imagen_t* cargarImagenPrincipal(imagen_t* imagen);
+imagen_t** cargarImagenesBuscar(imagen_t** arreglo,int cantidadImagenes);
+void liberarImagenesArreglo(imagen_t** arregloImagenes,int cantidadImagenes);
+void imprimirImagenesArreglo(imagen_t** arregloImagenes,int cantidadImagenes);
